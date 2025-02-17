321b281b0210c3116013286724a270e39bb64f59
Daniela Rodrigues <dani3lacampos@gmail.com>
2025-02-17 17:45:31 +0000

    mensagem

---
+++ app/src/main/java/application/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository
+
+import application.model.Categoria;
+
+public interface CategoriaRepository extens CrudRepository<Categoria, Long> {
+    
+}
